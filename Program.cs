e31bd87952e2ac45dce8f9622a330e5a678087a5
Dawson Huang <dawson09@163.com>
2024-07-28 16:09:40 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add web controller support
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
