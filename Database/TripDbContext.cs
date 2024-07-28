e07098ed67d1c461efab8a480f194435a808af55
Dawson Huang <dawson09@163.com>
2024-07-28 16:59:25 +0800

    Add data seeding

--- Database/TripDbContext.cs
+++ Database/TripDbContext.cs
@@ -1,5 +1,7 @@
 ﻿using Dawson.Trip.Api.Models;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Reflection;
 
 namespace Dawson.Trip.Api.Database;
 
@@ -12,4 +14,17 @@ public class TripDbContext : DbContext
     public DbSet<TouristRoute> TouristRoutes { get; set; }
 
     public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var routesData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) +
+            @"/Database/tourist-routes.json");
+        var routes = JsonConvert.DeserializeObject<IList<TouristRoute>>(routesData);
+        modelBuilder.Entity<TouristRoute>().HasData(routes);
+
+        var routePicturesData = File.ReadAllText(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) +
+            @"/Database/tourist-route-pictures.json");
+        var routePictures = JsonConvert.DeserializeObject<IList<TouristRoutePicture>>(routePicturesData);
+        modelBuilder.Entity<TouristRoutePicture>().HasData(routePictures);
+    }
 }
