dd593fbeac3c657511192f999ecd0c9467ad8709
Dawson Huang <dawson09@163.com>
2024-07-28 23:13:58 +0800

    Update tourist route schema

---
+++ Models/DepartureCity.cs
@@ -0,0 +1,9 @@
+﻿namespace Dawson.Trip.Api.Models;
+
+public enum DepartureCity
+{
+    Beijing,
+    Shanghai,
+    Shenzhen,
+    Canton
+}
---
+++ Models/TripDays.cs
@@ -0,0 +1,14 @@
+﻿namespace Dawson.Trip.Api.Models;
+
+public enum TripDays
+{
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    EightMore
+}
