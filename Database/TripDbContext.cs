1ca558107d6ac8dd40259fc0e0cf8c05c976bde7
Dawson Huang <dawson09@163.com>
2024-07-28 16:39:03 +0800

    Add database context

---
+++ Database/TripDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Dawson.Trip.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dawson.Trip.Api.Database;
+
+public class TripDbContext : DbContext
+{
+    public TripDbContext(DbContextOptions<TripDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<TouristRoute> TouristRoutes { get; set; }
+
+    public DbSet<TouristRoutePicture> TouristRoutePictures { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Dawson.Trip.Api.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add web controller support
 builder.Services.AddControllers();
+
+// Add database context support
+builder.Services.AddDbContext<TripDbContext>(option =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("SqlServer");
+
+    option.UseSqlServer(connectionString);
+});
 
 var app = builder.Build();
 
