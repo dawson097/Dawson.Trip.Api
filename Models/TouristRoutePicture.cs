c39f7808dfffcc233c623a93c84ac68e20d8b0f0
Dawson Huang <dawson09@163.com>
2024-07-28 16:23:13 +0800

    Add data model

---
+++ Models/TouristRoutePicture.cs
@@ -0,0 +1,12 @@
+﻿namespace Dawson.Trip.Api.Models;
+
+public class TouristRoutePicture
+{
+    public int Id { get; set; }
+
+    public string Url { get; set; }
+
+    public Guid TouristRouteId { get; set; }
+
+    public TouristRoute TouristRoute { get; set; }
+}
